e869bccac5ab7e61eba66f24bb61cf8735d02b7d
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-24 22:58:45 +0200

    Added title and FirstQuestion.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Quiz by Rina</h1>
+      <h2>Are you sure you know all the answer?</h2>
+    </div>
+  );
 }
 
 export default App;
